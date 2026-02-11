f9765bfdc524a3673faab56c99393046576da2a6
Ajeet Chaurasiya <cvajeetchaurasiya8887@gmail.com>
2026-02-11 11:51:14 +0530

    ..

--- classroom DS/linkedlistprogram/linkedsumnumber.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdlib.h>
-#include"headerfiles/myheaderfile.h"
-/****************************/
-int Sumnumbers(node*s1,node*s2){
-    node*p=reverse(s1);
-    node*q=reverse(s2);
-    return 0;
-}
